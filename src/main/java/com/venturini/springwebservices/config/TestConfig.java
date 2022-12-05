7b0f57a7f8177303b20166de1330219695c65d1d
Marcello Venturini <marcellofventurini@gmail.com>
2022-12-04 23:31:34 -0300

    Create OrderStatus enum

--- src/main/java/com/venturini/springwebservices/config/TestConfig.java
+++ src/main/java/com/venturini/springwebservices/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.venturini.springwebservices.config;
 
 import com.venturini.springwebservices.entities.Order;
 import com.venturini.springwebservices.entities.User;
+import com.venturini.springwebservices.entities.enums.OrderStatus;
 import com.venturini.springwebservices.repositories.OrderRepository;
 import com.venturini.springwebservices.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,9 @@ public class TestConfig implements CommandLineRunner {
     User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
     User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-    Order o1 = new Order(null, Instant.parse("2022-06-20T19:22:07Z"), u1);
-    Order o2 = new Order(null, Instant.parse("2022-07-21T03:33:07Z"), u2);
-    Order o3 = new Order(null, Instant.parse("2022-07-22T15:53:07Z"), u1);
+    Order o1 = new Order(null, Instant.parse("2022-06-20T19:22:07Z"), OrderStatus.PAID, u1);
+    Order o2 = new Order(null, Instant.parse("2022-07-21T03:33:07Z"), OrderStatus.WAITING_PAYMENT, u2);
+    Order o3 = new Order(null, Instant.parse("2022-07-22T15:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
 
     userRepository.saveAll(Arrays.asList(u1, u2));
     orderRepository.saveAll(Arrays.asList(o1, o2, o3));
