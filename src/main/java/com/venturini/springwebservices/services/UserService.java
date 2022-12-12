c28dcb354f973c178f90ba7daf18d333d033ac2d
Marcello Venturini <marcellofventurini@gmail.com>
2022-12-11 23:33:45 -0300

    Exception handling - update

--- src/main/java/com/venturini/springwebservices/services/UserService.java
+++ src/main/java/com/venturini/springwebservices/services/UserService.java
@@ -4,6 +4,7 @@ import com.venturini.springwebservices.entities.User;
 import com.venturini.springwebservices.repositories.UserRepository;
 import com.venturini.springwebservices.services.exceptions.DataBaseException;
 import com.venturini.springwebservices.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +44,13 @@ public class UserService {
   }
 
   public User update(Long id, User user) {
-    User entity = userRepository.getReferenceById(id);
-    updateData(entity, user);
-    return userRepository.save(entity);
+    try {
+      User entity = userRepository.getReferenceById(id);
+      updateData(entity, user);
+      return userRepository.save(entity);
+    } catch (EntityNotFoundException e) {
+      throw new ResourceNotFoundException(id);
+    }
   }
 
   private void updateData(User entity, User user) {
